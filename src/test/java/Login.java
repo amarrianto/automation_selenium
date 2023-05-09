a1fd0b82d7605eedb4cdfdfbdc9ecc8f39711b61
amarrianto <amarrianto6969@gmail.com>
2023-05-09 23:04:11 +0700

    test

---
+++ src/test/java/Login.java
@@ -0,0 +1,61 @@
+import config.env_target;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Login extends env_target {
+    @Test
+    public void main() {
+        // Set driverlocation path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+        // Maximize driver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        // Set Url
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'][@data-test='login-button']"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@type='submit'][@data-test='login-button']")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title'][contains(text(),'Products')]"))
+        );
+//        driver.quit();
+    }
+
+    @Test
+    public void failedTest() {
+        // Set driverlocation path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+        // Maximize driver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        // Set Url
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='submit'][@data-test='login-button']"))
+        );
+        driver.findElement(By.name("user-name")).sendKeys("standard");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@type='submit'][@data-test='login-button']")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\'login_button_container\']/div/form/div[3]"))
+        );
+        driver.quit();
+    }
+}
